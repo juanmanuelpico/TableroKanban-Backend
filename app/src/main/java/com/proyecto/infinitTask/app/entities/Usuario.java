d908436cd52b157d72fceae758211ced7fb6da0a
juanmanuelpico <picojuan322@gmail.com>
2023-12-06 11:52:31 -0300

    conectado a la bd

--- app/src/main/java/com/proyecto/infinitTask/app/entities/Usuario.java
+++ app/src/main/java/com/proyecto/infinitTask/app/entities/Usuario.java
@@ -1,5 +1,6 @@
 package com.proyecto.infinitTask.app.entities;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,15 +12,28 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @Getter
 @Setter
+@Entity
+@Table(name="usuario")
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_usuario")
     private int id;
+    @Column(name="usuario")
     private String usuario;
+    @Column(name="password")
     private String password;
+    @Column(name="rol")
     private String rol;
+    @Column(name="email")
     private String email;
+    @Column(name="fechaAlta")
     private LocalDate fechaAlta;
+    @Column(name="fechaBaja")
     private LocalDate fechaBaja;
+    @Column(name="fechaActualizacion")
     private LocalDate fechaActualizacion;
+    @Column(name="activo")
     private boolean activo;
 
 }
