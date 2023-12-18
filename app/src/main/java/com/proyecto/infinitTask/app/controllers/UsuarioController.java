a242247de0773c931c4d79f1330c0a80c7c5fe78
juanmanuelpico <picojuan322@gmail.com>
2023-12-18 11:42:22 -0300

    mejora

--- app/src/main/java/com/proyecto/infinitTask/app/controllers/UsuarioController.java
+++ app/src/main/java/com/proyecto/infinitTask/app/controllers/UsuarioController.java
@@ -5,6 +5,9 @@ import com.proyecto.infinitTask.app.dtos.response.UsuarioDTOResponse;
 import com.proyecto.infinitTask.app.entities.Usuario;
 import com.proyecto.infinitTask.app.services.IUsuarioService;
 import com.proyecto.infinitTask.app.util.Mensaje;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,17 +22,22 @@ public class UsuarioController {
     @Autowired
     private IUsuarioService usuarioService;
 
+
+   //Este controlador recibe un body desde el front y lo convierte a UsuarioDTORequest
+   //Luego convierte el dto a entidad para guardarlo en la base de datos
+    //Finalmente devuelve un body para que el front puede saber  travez de una msj si el
+    //usuario fue ingresado correctamente
     @PostMapping("/registro")
     public ResponseEntity<Object> crearUsuario(@RequestBody UsuarioDTORequest dto){
         try{
            usuarioService.crearUsuario(dto);
            return ResponseEntity.status(HttpStatus.CREATED).body(new Mensaje("Usuario creado exitosamente"));
-           //ES QUIVALENTE
+           //ES EQUIVALENTE
            //return new ResponseEntity<>(new Mensaje("Usuario creado exitosamente"), HttpStatus.CREATED );
 
         }catch(Exception e){
             return new ResponseEntity<>(new Mensaje(e.getMessage()) , HttpStatus.BAD_REQUEST);
-            //ES QUIVALENTE
+            //ES EQUIVALENTE
            // return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Mensaje(e.getMessage()));
         }
     }
